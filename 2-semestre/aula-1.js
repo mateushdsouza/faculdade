1edfa2d63a023522dcbe26b9fd69390eabb4d176
Mateus de Souza <teushdsouza@gmail.com>
2025-07-24 15:00:26 -0300

    Commit inicial

---
+++ 2-semestre/aula-1.js
@@ -0,0 +1,13 @@
+let aluno = "maria";
+let curso1 = "ADS";
+let curso2 = "Engenharia de Software";
+
+if (aluno == "joao") {
+    console.log("O curso é " + curso1);
+}else if (aluno == "maria") {
+    console.log("O curso é " + curso2);
+}
+else {
+    console.log("Aluno não encontrado");
+}
+
